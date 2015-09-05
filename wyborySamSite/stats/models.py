8f3406184283370f8b40a2ea99050d8a306ec2c6
Paweł Gałwa <pawel@Pawes-MacBook-Pro.local>
2015-09-05 18:37:15 +0200

    models created

--- wyborySamSite/stats/models.py
+++ wyborySamSite/stats/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    teryt = models.IntegerField(default=0)
+    number_of_district = models.IntegerField(default=0)
+    address = models.CharField(max_length=500, defualt=None)
+    district = models.CharField(max_length=50, default=None)
+    commune = models.CharField(max_length=200, default=None)
+    commune_type = models.CharField(max_length=200, default=None)
+    county = models.CharField(max_length=200, default=None)
+    voivodeship = models.CharField(max_length=200, default=None)
+    number_of_electoral_circuit = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class Votes_data(Address):
+    type = models.CharField(max_length=10, default=None)
+    number_of_voters = models.IntegerField(default=0)
+    number_of_proxies = models.IntegerField(default=0)
+    cards_given = models.IntegerField(default=0)
+    cards_taken = models.IntegerField(default=0)
+    cards_taken_from_box = models.IntegerField(default=0)
+    votes_valid = models.IntegerField(default=0)
+    votes_invalid = models.IntegerField(default=0)
+    cards_received = models.IntegerField(default=0)
+    cards_valid = models.IntegerField(default=0)
+    cards_invalid = models.IntegerField(default=0)
+    cards_invalid_x = models.IntegerField(default=0)
+    cards_invalid_xx = models.IntegerField(default=0)
+    cards_unused = models.IntegerField(default=0)
+    polish_citizens = models.IntegerField(default=0)
+    polish_citizens_b = models.IntegerField(default=0)
+    envelope_unsealed = models.IntegerField(default=0)
+    envelopes_thrown_into_box = models.IntegerField(default=0)
+    envelopes_without_statement = models.IntegerField(default=0)
+    envelopes_returned = models.IntegerField(default=0)
+    envelopes_returned_without_envelope = models.IntegerField(default=0)
+    unsigned_statements = models.IntegerField(default=0)
+    eu_citizens = models.IntegerField(default=0)
+    eu_citiznes_b = models.IntegerField(default=0)
+    electoral_packages = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class Election(Votes_data):
+    election_type = models.CharField(max_length=10, default=None)
+    votes = models.CharField(max_length=10000, default=None)
