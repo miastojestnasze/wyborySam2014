340f67e8a99b2d3bf4b3c0d8b60dc4a4873df13d
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-11-08 20:17:40 +0100

    urls improved

--- wyborySamSite/stats/urls.py
+++ wyborySamSite/stats/urls.py
@@ -1,20 +1,27 @@
 from django.conf.urls import url
 from stats.views import import_jsons, get_geography, get_areas_tree,\
-    get_district_stats, get_circle_stats, get_circuit_stats,\
-    get_political_party_candidates, get_circle_candidates
+    get_stats, get_candidates
 
 urlpatterns = [
-    url(r'^data/import/$', import_jsons, name='index'),
-    url(r'^api/stats/areas-tree/$', get_areas_tree, name='index'), # returns  json {''}
-    url(r'^api/stats/geography/$', get_geography, name='index'),
-    url(r'^api/stats/(?P<election_type>[a-z]+)/(?P<district_name>[\w|\W]+)/$',
-        get_district_stats, name='index'), # return_districts_data
-    url(r'^api/stats/(?P<election_type>[a-z]+)/(?P<district_name>[\w|\W]+)/(?P<circle>[1-9]+)/$',
-        get_circle_stats, name='index'), # return_circle_data
-    url(r'^api/stats/(?P<election_type>[a-z]+)/(?P<district_name>[\w|\W]+)/(?P<circle>[1-9]+)/(?P<circuit>[1-9]+)/$',
-        get_circuit_stats, name='index'), #return_circuit_data
-    url(r'^api/candidates/(?P<election_type>[a-z]+)/(?P<district_name>[\w|\W]+)/(?P<party>[\w|\W]+)/$', 
-        get_political_party_candidates, name='index'),
-    url(r'^api/candidates/(?P<election_type>[a-z]+)/(?P<district_name>[\w|\W]+)/(?P<circle>[1-9]+)/$', 
-        get_circle_candidates, name='index'),
+    url(r'^data/import/$', import_jsons, name='data-import'),
+    url(r'^api/stats/areas-tree/$', get_areas_tree, name='areas-tree'), # returns  json {''}
+    url(r'^api/stats/geography/$', get_geography, name='geography'),
+    url(r'^api/stats/(?P<election_type>.+)/district-(?P<district>[\w|\W]+)/circle-(?P<number_of_district>[1-9]+)/circuit-(?P<number_of_electoral_circuit>[1-9]+)/$',
+        get_stats, name='electoral-circuit-stats'),
+    url(r'^api/stats/(?P<election_type>.+)/district-(?P<district>[\w|\W]+)/circle-(?P<number_of_district>[1-9]+)/$',
+        get_stats, name='electoral-circle-stats'),
+    url(r'^api/stats/(?P<election_type>.+)/circle-(?P<number_of_district>[1-9]+)/$',
+        get_stats, name='district-stats'),
+    url(r'^api/stats/(?P<election_type>.+)/circle-(?P<number_of_district>[1-9]+)/circuit-(?P<number_of_electoral_circuit>[1-9]+)/$',
+        get_stats, name='district-stats'),
+    url(r'^api/stats/(?P<election_type>.+)/district-(?P<district>[\w|\W]+)/circuit-(?P<number_of_electoral_circuit>[1-9]+)$',
+        get_stats, name='district-stats'), 
+    url(r'^api/stats/(?P<election_type>.+)/district-(?P<district>[\w|\W]+)/$',
+        get_stats, name='district-stats'), 
+    
+    url(r'^api/stats/(?P<election_type>.+)/$', get_stats, name='stats'),
+    url(r'^api/candidates/(?P<election_type>[a-z]+)/(?P<district>[\w|\W]+)/(?P<party>[\w|\W]+)/$', 
+        get_candidates, name='candidates'),
+    url(r'^api/candidates/(?P<election_type>[a-z]+)/(?P<district>[\w|\W]+)/(?P<circle>[1-9]+)/$', 
+        get_candidates, name='index'),
 ]
