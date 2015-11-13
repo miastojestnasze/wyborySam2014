930c755053429fb8a1546fe8eeec421586671a0d
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-11-13 16:06:07 +0100

    heroku deploy test #1

--- wyborySamSite/wyborySamSite/settings.py
+++ wyborySamSite/wyborySamSite/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000'
