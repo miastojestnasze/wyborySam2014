e7ed2ec1e01d8a726a92f55a8111c210b39df08c
Paweł Gałwa <pawelgalwa@Pawels-MacBook-Pro.local>
2015-11-15 18:54:05 +0100

    endpoint for dataenpoint freezed

--- stats/urls.py
+++ stats/urls.py
@@ -3,7 +3,7 @@ from stats.views import import_jsons, get_geography, get_areas_tree,\
     get_stats, get_candidates
 
 urlpatterns = [
-    url(r'^data/import/$', import_jsons, name='data-import'),
+    # url(r'^data/import/$', import_jsons, name='data-import'),
     url(r'^api/stats/areas-tree/$', get_areas_tree, name='areas-tree'), # returns  json {''}
     url(r'^api/stats/geography/$', get_geography, name='geography'),
     url(r'^api/stats/(?P<election_type>.+)/district-(?P<district>[\w|\W]+)/circle-(?P<number_of_district>[0-9]+)/circuit-(?P<number_of_electoral_circuit>[0-9]+)/$',
